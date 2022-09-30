204f8d54c2d792ff3b2cef6323f2cf97ff3214ac
cjheyde <carlaheyde@gmail.com>
2022-09-30 01:29:18 -0300

    fix: :bug: Dockerfile backend change and created users mu=igration unsing npx

--- app/backend/src/database/migrations/20220930042213-create-users.js
+++ app/backend/src/database/migrations/20220930042213-create-users.js
@@ -1,6 +1,7 @@
+'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('users', {
+await queryInterface.createTable('users', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -26,7 +27,8 @@ module.exports = {
       },
     });
   },
-  down: async (queryInterface) => {
+
+  down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('users');
-  },
+  }
 };
