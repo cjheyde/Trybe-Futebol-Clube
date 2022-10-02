079bccf229ad1872a5717a2391ea3b7a4e7a0b8b
cjheyde <carlaheyde@gmail.com>
2022-10-02 05:04:03 -0300

    feat: :test_tube: test written for /teams GET route

---
+++ app/backend/src/tests/teams.integration.test.ts
@@ -0,0 +1,48 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import Team from '../database/models/TeamModel';
+
+import { Response } from 'superagent';
+import { response } from "express";
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+const teamsTest = [
+	{
+		"id": 1,
+		"teamName": "Avaí/Kindermann"
+	},
+	{
+		"id": 2,
+		"teamName": "Bahia"
+	},
+]
+
+describe('route /teams', () => {
+  describe('route /teams - GET is successfully done', () => {
+    let chaiHttpResponse: Response;
+
+    before(async () => {
+      sinon
+        .stub(Team, "findAll")
+        .resolves(teamsTest as Team[]);
+    });
+
+    after(()=>{
+      sinon.restore();
+    });
+
+    it('get successfully all teams from database', async () => {
+      chaiHttpResponse = await chai
+         .request(app).get('/teams').send();
+      chai.expect(response.status).to.equal(200);
+      chai.expect(response.json).to.equal(teamsTest);
+    });
+  });
+});
