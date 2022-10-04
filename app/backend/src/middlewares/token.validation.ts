5cc5521e46220f6b9c3f9022ca3691037094fd2a
cjheyde <carlaheyde@gmail.com>
2022-10-04 16:32:03 -0300

    feat: :construction: Created Token Validation

---
+++ app/backend/src/middlewares/token.validation.ts
@@ -0,0 +1,15 @@
+import { RequestHandler } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { verifyToken } from '../helpers/token';
+
+const tokenValidation: RequestHandler = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Token not valid' });
+  }
+  const token = verifyToken(authorization);
+  req.body.loggedUser = token;
+  next();
+};
+
+export default tokenValidation;
