9497a8db0a4db791de1aaa51ee58bf4e51739acd
cjheyde <carlaheyde@gmail.com>
2022-10-06 04:15:05 -0300

    feat: :white_check_mark: /matches POST - includded condition that teams cannot play against themselves

--- app/backend/src/middlewares/matches.validation.ts
+++ app/backend/src/middlewares/matches.validation.ts
@@ -6,6 +6,10 @@ const matchesValidation: RequestHandler = (req, res, next) => {
   if (!homeTeam || !awayTeam || !homeTeamGoals || !awayTeamGoals) {
     return res.status(StatusCodes.BAD_REQUEST).json({ message: 'All fields must be filled' });
   }
+  if (homeTeam === awayTeam) {
+    return res.status(StatusCodes.UNAUTHORIZED)
+      .json({ message: 'It is not possible to create a match with two equal teams' });
+  }
 
   next();
 };
